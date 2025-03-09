4d9cc65c107ee0203ee07a07d063ed2b4d1ac3ab
i.ilinykh <i.ilinykh@quattrolab.ru>
2025-03-09 18:38:20 +0300

    copilot сука

--- DZ 06.03.25.cpp
+++ DZ 06.03.25.cpp
@@ -14,7 +14,8 @@ int main()
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
-	int nums[10];
+	int nums[] = { 0,0,0,0,0,0,0,0,0,0 };
+	int changeNums[] = { 0,0,0,0,0,0,0,0,0,0 };
 
 	int start = 1;
 	int end = 9;
@@ -22,25 +23,26 @@ int main()
 	int maxItem = 0;
 	int minItem = 9;
 
-	int changeNums[10] = {0,0,0,0,0,0,0,0,0,0};
+	int maxItemCount = 0;
+	int minItemCount = 0;
 
+	srand(time(0) + clock());
 
 	for (int i = 0; i < 10; i++)
 	{
-		srand(time(0) + clock());
-	
 		nums[i] = rand() % (end - start + 1) + start;
 		cout << endl << "Элемент массива номер " << i + 1 << " равен " << nums[i] << endl;
+
 
 		if (nums[i] > maxItem)
 		{
 			maxItem = nums[i];
 		}
-		
+
 		if (nums[i] < minItem)
 		{
-			minItem = nums[i];	
-		}	
+			minItem = nums[i];
+		}
 
 		switch (nums[i])
 		{
@@ -62,24 +64,40 @@ int main()
 			break;
 		case 9: changeNums[8] += 1;
 			break;
-			default : cout << endl << "Случилось очко какое-то!" << endl; 
+		default: cout << endl << "Случилось очко какое-то!" << endl;
 			break;
 		}
 	}
 	cout << endl << "************************************************************* " << endl;
-
-	cout << endl << "Максимальное значение элемента в этом массиве: " << maxItem  << endl;
-	cout << endl << "Минимальное значение элемента в этом массиве: " << minItem  << endl;
-
-	cout << endl << "************************************************************* " << endl;
-
-	for (int j =0; j<9; j++) 
-	{
-		cout << endl << "Количество элементов в массиве со значением "<< j + 1 << " равно " << changeNums[j] << endl;
-	}
-
 	
+	cout << endl << "Mассив nums содержит значения: [";
+	for (auto e : nums)
+	{
+		cout << " " << e;
+	}
+	cout << " ] " << endl;
 
-
-
+	cout << endl << "Максимальное значение элемента в этом массиве: " << maxItem << endl;
+	cout << endl << "Минимальное значение элемента в этом массиве: " << minItem << endl;
+	cout << endl << "************************************************************* " << endl;
+	
+	for (int j = 0; j < 9; j++)
+	{
+		cout << endl << "Количество элементов в массиве со значением " << j + 1 << " равно " << changeNums[j] << endl;
+	}
+	cout << endl << "************************************************************* " << endl;
+	
+	for (int c = 0; c < 10; c++)
+	{
+		if (maxItem == nums[c])
+		{
+			maxItemCount++;
+		}
+		if (minItem == nums[c])
+		{
+			minItemCount++;
+		}
+    }
+	cout << endl << "Количество элементов с максимальным значением " << maxItem << " равно: " << maxItemCount << endl;
+	cout << endl << "Количество элементов с минимальным значением " << minItem << " равно: " << minItemCount << endl;
 }
